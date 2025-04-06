f93db792cd505f49cf2d84a9ca48c121d22fcfe4
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2025-04-06 15:48:10 -0300

    feat: adiciona manipulador de erros com validação para ZodError

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,31 @@
 import fastify from 'fastify'
+import { ZodError } from 'zod'
 
-import { userRoutes } from './routes/user'
+import { env } from './env'
+import { Environment } from './env/utils'
+import { appRoutes } from './http/Routes/user'
 
 export const app = fastify()
 
-app.register(userRoutes, { prefix: '/user' })
+app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.flatten().fieldErrors,
+    })
+  }
+
+  if (env.NODE_ENV !== Environment.PROD) {
+    console.error(error)
+  } else {
+    // TODO: log to an external service
+    // e.g. Sentry, LogRocket, etc.
+    // log.error(error)
+  }
+
+  return reply.status(500).send({
+    message: 'Internal server error',
+  })
+})
