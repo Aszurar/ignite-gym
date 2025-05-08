998cb2f7c09464c5d65cac1f3ac2abf62f1b9408
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2025-05-08 13:36:52 -0300

    feat: adiciona rotas para autenticação e recuperação de usuário

--- src/http/Routes/user.ts
+++ src/http/Routes/user.ts
@@ -1,9 +1,17 @@
 import { FastifyInstance } from 'fastify'
 
+import { AuthenticateUserController } from '../Controllers/authenticate-user.controller'
+import { GetUserController } from '../Controllers/get-user.controller'
 import { RegisterUserController } from '../Controllers/register-user.controller'
 
 export async function userRoutes(app: FastifyInstance) {
   const registerUserController = new RegisterUserController()
+  const authenticateUserController = new AuthenticateUserController()
+  const getUserController = new GetUserController()
 
   app.post('/users', registerUserController.handle)
+
+  app.post('/sessions', authenticateUserController.handle)
+
+  app.get('/users/:id', getUserController.handler)
 }
