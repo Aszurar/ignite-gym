abbed73531db3f16c51c0a68ef56c7fca622ba55
LucasSouza.idr <lms.souza39@gmail.com>
2025-06-03 15:38:16 -0300

    feat: adiciona métodos findById e update na interface ICheckInRepository

--- src/Repositories/interfaces/check-in.repository.ts
+++ src/Repositories/interfaces/check-in.repository.ts
@@ -1,8 +1,10 @@
 import { CheckIn, Prisma } from '@prisma/client'
 
 export interface ICheckInRepository {
+  findById(checkInId: string): Promise<CheckIn | null>
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
   countByUserId(userId: string): Promise<number>
+  update(checkIn: CheckIn): Promise<CheckIn>
 }
