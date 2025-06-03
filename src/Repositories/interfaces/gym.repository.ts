c4f087ecc7e626b050ee07c763bf1e2dd340e1a3
LucasSouza.idr <lms.souza39@gmail.com>
2025-06-03 12:55:01 -0300

    feat: adiciona métodos de busca e localização de academias na interface IGymRepository

--- src/Repositories/interfaces/gym.repository.ts
+++ src/Repositories/interfaces/gym.repository.ts
@@ -1,6 +1,13 @@
 import { Gym, Prisma } from '@prisma/client'
 
+export type FindManyNearbyParamsType = {
+  latitude: number
+  longitude: number
+}
+
 export interface IGymRepository {
   findById(id: string): Promise<Gym | null>
   create(data: Prisma.GymCreateInput): Promise<Gym>
+  searchMany(query: string, page: number): Promise<Gym[]>
+  findManyNearby(params: FindManyNearbyParamsType, page: number): Promise<Gym[]>
 }
