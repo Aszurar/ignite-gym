36185d07a5c3ae858fc6dff62c5ad381d337dca9
LucasSouza.idr <lms.souza39@gmail.com>
2025-06-04 16:19:23 -0300

    feat: reorganiza métodos na classe PrismaGymRepository para melhorar a legibilidade e a estrutura do código

--- src/Repositories/prisma/prisma-gym.repository.ts
+++ src/Repositories/prisma/prisma-gym.repository.ts
@@ -1,22 +1,53 @@
-import { Prisma } from '@prisma/client'
+import { Gym, Prisma } from '@prisma/client'
 
 import { prisma } from '@/lib/prisma'
 
-import { IGymRepository } from '../interfaces/gym.repository'
+import {
+  FindManyNearbyParamsType,
+  IGymRepository,
+} from '../interfaces/gym.repository'
 
 class PrismaGymRepository implements IGymRepository {
-  async create(data: Prisma.GymCreateInput) {
-    const gym = await prisma.gym.create({ data })
-
-    return gym
-  }
-
   async findById(id: string) {
     const gym = await prisma.gym.findUnique({
       where: {
         id,
       },
     })
+
+    return gym
+  }
+
+  async findManyNearby(
+    { latitude, longitude }: FindManyNearbyParamsType,
+    page: number,
+  ) {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * from gyms
+      WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+      LIMIT 20 OFFSET ${(page - 1) * 20}
+    `
+
+    return gyms
+  }
+
+  async searchMany(query: string, page: number): Promise<Gym[]> {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+          mode: 'insensitive',
+        },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return gyms
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = await prisma.gym.create({ data })
 
     return gym
   }
