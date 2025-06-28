ad1a7f1792d279bf649630142717dc1ad70445ec
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2025-06-28 20:31:42 -0300

    feat: adiciona configuração de testes unitários e e2e no Vite, incluindo diretórios e ambiente de teste

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,26 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+
+  test: {
+    dir: 'src',
+    workspace: [
+      {
+        extends: true,
+        test: {
+          name: 'unit',
+          dir: 'src/UseCases/tests',
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'e2e',
+          dir: 'src/http/tests-e2e',
+          environment:
+            './prisma/vitest-environment-prisma/prisma-test-environment.ts',
+        },
+      },
+    ],
+  },
 })
